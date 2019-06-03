dea9753fafad3cf06947f0ce548fc5b7f104b45f
FedeG <federicogonzalez761@gmail.com>
2019-06-03 05:55:10 -0300

    Update clock practice

--- src/practice/clock/index.jsx
+++ src/practice/clock/index.jsx
@@ -2,15 +2,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+class Clock extends React.PureComponent {
+  state = {
+    date: new Date()
+  }
+
+  componentDidMount() {
+    setInterval(() => this.setState({date: new Date()}), 1000)
+  }
+
+  handleOnClick = event => {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log(this.props.name);
+  }
+
+  render() {
+    const showTitle = this.props.name && this.props.name.length > 0;
+    return (
+      <div onClick={this.handleOnClick}>
+        {showTitle && (<h1>{this.props.name}</h1>)}
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    )
+  }
 }
 
-// Llama a ReactDOM.render() cada 1000 milisegundo.
-setInterval(tick, 1000);
+class Clocks extends React.PureComponent {
+  render() {
+    const clocks = this.props.names.map(name => <Clock name={name} key={name} />);
+    return <div>{clocks}</div>;
+  }
+}
+
+ReactDOM.render(<Clocks names={['clock 1', 'clock 3', 'clock 2']} />, document.getElementById('root'));
